c178f429e3f2e985132262ce98b5fbad31ff5ce7
Noël Vissers <noel.vissers@prodrive-technologies.com>
2025-08-04 11:41:16 +0200

    Create MIDI classes

--- include/midiHandler.h
+++ include/midiHandler.h
@@ -3,6 +3,19 @@
 class CMidiHandler
 {
 public:
-  void Example();
+  void init();
+
+  // MIDI data messages
+  void midiNoteOff();
+  void midiNoteOn();
+  void midiControlChange();
+  void midiChannelAfterTouch();
+  void midiPitchWheel();
+
+  // MIDI system messages
+  void systemTimingClock();
+  void systemStart();
+  void systemStop();
+  void systemReset();
 private:
 };
--- src/midiHandler.cpp
+++ src/midiHandler.cpp
@@ -1,15 +1,54 @@
 #include <Arduino.h>
 #include "midiHandler.h"
 
-// --------------------------------------------------
-// Public
-// --------------------------------------------------
-
-void CMidiHandler::Example()
+void CMidiHandler::init()
 {
     
 }
 
-// --------------------------------------------------
-// Private
-// --------------------------------------------------
+// MIDI data messages
+void CMidiHandler::midiNoteOff()
+{
+    
+}
+
+void CMidiHandler::midiNoteOn()
+{
+    
+}
+
+void CMidiHandler::midiControlChange()
+{
+    
+}
+
+void CMidiHandler::midiChannelAfterTouch()
+{
+    
+}
+
+void CMidiHandler::midiPitchWheel()
+{
+    
+}
+
+// MDII system messages
+void CMidiHandler::systemTimingClock()
+{
+    
+}
+
+void CMidiHandler::systemStart()
+{
+    
+}
+
+void CMidiHandler::systemStop()
+{
+    
+}
+
+void CMidiHandler::systemReset()
+{
+    
+}
