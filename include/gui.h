a845310fef4fca6b59cd31fdb650ad2c8caf9f2c
Noël Vissers <noel.vissers@prodrive-technologies.com>
2025-08-05 11:28:23 +0200

    Delete files

--- include/gui.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class CGui
-{
-public:
-  void Example();
-private:
-};
--- include/midiSerial.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class CMidiSerial
-{
-public:
-  void init();
-  void update();
-private:
-};
