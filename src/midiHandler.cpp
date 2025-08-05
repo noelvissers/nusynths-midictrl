68ca3b22c2a963483b9a4f7faede2c417fad39a2
Noël Vissers <noel.vissers@prodrive-technologies.com>
2025-08-05 11:21:13 +0200

    change tab size

--- src/midiHandler.cpp
+++ src/midiHandler.cpp
@@ -10,128 +10,128 @@ void CMidiHandler::init()
 // MIDI data messages
 void CMidiHandler::midiNoteOff(byte note, byte velocity)
 {
-	COutputs outputs;
+  COutputs outputs;
 
-	for (auto &output : outputs._outputs)
-	{
-		if (output.isActive)
-		{
-			switch (output.function)
-			{
-			case OutputFunction::Pitch:
-				if (output.value == note)
-					output.isActive = false;
-				break;
-			case OutputFunction::Velocity:
-				if (output.mappedNote == note)
-					output.isActive = false;
-				break;
-			case OutputFunction::Gate:
-				if (output.mappedNote == note)
-				{
-					output.value = 0;
-					output.isActive = false;
-					output.isDirty = true;
-				}
-				break;
-			default:
-				break;
-			}
-		}
-	}
+  for (auto &output : outputs._outputs)
+  {
+    if (output.isActive)
+    {
+      switch (output.function)
+      {
+      case OutputFunction::Pitch:
+        if (output.value == note)
+          output.isActive = false;
+        break;
+      case OutputFunction::Velocity:
+        if (output.mappedNote == note)
+          output.isActive = false;
+        break;
+      case OutputFunction::Gate:
+        if (output.mappedNote == note)
+        {
+          output.value = 0;
+          output.isActive = false;
+          output.isDirty = true;
+        }
+        break;
+      default:
+        break;
+      }
+    }
+  }
 }
 
 void CMidiHandler::midiNoteOn(byte note, byte velocity)
 {
-	COutputs outputs;
+  COutputs outputs;
 
-	for (auto &output : outputs._outputs)
-	{
-		if (!output.isActive)
-		{
-			switch (output.function)
-			{
-			case OutputFunction::Pitch:
-				output.value = outputs.midiTo1VOct(note);
-				output.mappedNote = note; // Use mappedNote for checking what note triggered this output
-				output.isActive = output.isDirty = true;
-				break;
-			case OutputFunction::Velocity:
-				output.value = outputs.midiToCv(velocity);
-				output.mappedNote = note; // Use mappedNote for checking what note triggered this output
-				output.isActive = output.isDirty = true;
-				break;
-			case OutputFunction::Gate:
-				output.value = 32768;			// 5V for DAC, HIGH for IO
-				output.mappedNote = note; // Use mappedNote for checking what note triggered this output
-				output.isActive = output.isDirty = true;
-				break;
-			case OutputFunction::Trigger:
-				if (!output.isMapped)
-				{
-					output.value = 32768;			// 5V for DAC, HIGH for IO
-					output.resetTime = micros() + (_triggerLengthMs * 1000);
-					output.mappedNote = note; // Use mappedNote for checking what note triggered this output
-					output.isActive = true;
-				}
-				else if (output.mappedNote == note)
-				{
-					output.value = 32768;			// 5V for DAC, HIGH for IO
-					output.resetTime = micros() + (_triggerLengthMs * 1000);
-					output.mappedNote = note; // Use mappedNote for checking what note triggered this output
-					output.isActive = true;
-				}
-				else
-					break;
-				output.isDirty = true;
-				break;
+  for (auto &output : outputs._outputs)
+  {
+    if (!output.isActive)
+    {
+      switch (output.function)
+      {
+      case OutputFunction::Pitch:
+        output.value = outputs.midiTo1VOct(note);
+        output.mappedNote = note; // Use mappedNote for checking what note triggered this output
+        output.isActive = output.isDirty = true;
+        break;
+      case OutputFunction::Velocity:
+        output.value = outputs.midiToCv(velocity);
+        output.mappedNote = note; // Use mappedNote for checking what note triggered this output
+        output.isActive = output.isDirty = true;
+        break;
+      case OutputFunction::Gate:
+        output.value = 32768;     // 5V for DAC, HIGH for IO
+        output.mappedNote = note; // Use mappedNote for checking what note triggered this output
+        output.isActive = output.isDirty = true;
+        break;
+      case OutputFunction::Trigger:
+        if (!output.isMapped)
+        {
+          output.value = 32768; // 5V for DAC, HIGH for IO
+          output.resetTime = micros() + (_triggerLengthMs * 1000);
+          output.mappedNote = note; // Use mappedNote for checking what note triggered this output
+          output.isActive = true;
+        }
+        else if (output.mappedNote == note)
+        {
+          output.value = 32768; // 5V for DAC, HIGH for IO
+          output.resetTime = micros() + (_triggerLengthMs * 1000);
+          output.mappedNote = note; // Use mappedNote for checking what note triggered this output
+          output.isActive = true;
+        }
+        else
+          break;
+        output.isDirty = true;
+        break;
 
-			default:
-				break;
-			}
+      default:
+        break;
+      }
 
-			// Exit after first active output in polyphonic mode
-			// Note: If multiple triggers is mapped to the same note, only 1 will be triggered in polyphonic mode
-			if (output.isDirty && _synthMode == SynthMode::Polyphonic)
-				return;
-		}
-	}
+      // Exit after first active output in polyphonic mode
+      // Note: If multiple triggers is mapped to the same note, only 1 will be triggered in polyphonic mode
+      if (output.isDirty && _synthMode == SynthMode::Polyphonic)
+        return;
+    }
+  }
 }
 
 void CMidiHandler::midiControlChange(byte byte1, byte byte2)
 {
-	// Handle continuous controller
+  // Handle continuous controller
 }
 
 void CMidiHandler::midiAfterTouchChannel(byte velocity)
 {
-	// Handle aftertouch
+  // Handle aftertouch
 }
 
 void CMidiHandler::midiPitchBend(int value)
 {
-	// Handle pitch
+  // Handle pitch
 }
 
 // MIDI system messages
 void CMidiHandler::systemClock()
 {
-	// Handle clock/sync
+  // Handle clock/sync
 }
 
 void CMidiHandler::systemStart()
 {
-	// Handle start
+  // Handle start
 }
 
 void CMidiHandler::systemStop()
 {
-	// Handle stop
+  // Handle stop
 }
 
 void CMidiHandler::systemReset()
 {
-	// Implement?
+  // Implement?
 }
 
 // TODO: Implement learn functionality
