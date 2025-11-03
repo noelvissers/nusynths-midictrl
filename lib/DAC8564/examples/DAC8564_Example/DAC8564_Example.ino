d4833a66ed3e2c17f6263fb3f634c79af1c51ed9
Noël Vissers <18530375+noelvissers@users.noreply.github.com>
2025-11-03 13:43:09 +0100

    Update DAC example

--- lib/DAC8564/examples/DAC8564_Example/DAC8564_Example.ino
+++ lib/DAC8564/examples/DAC8564_Example/DAC8564_Example.ino
@@ -4,39 +4,33 @@
 #define PIN_NSS 10 // SYNC pin on DAC8564
 #define PIN_LDAC 9
 #define PIN_ENABLE 8
-#define SPI_SPEED 1000000
+#define SPI_SPEED 24000000 // 24MHz
 
-DAC8564 dac(SPI, SPI_SPEED, PIN_NSS, PIN_LDAC, PIN_ENABLE);
+DAC8564 dac(SPI_SPEED, PIN_ENABLE, PIN_NSS);
 
 void setup()
 {
-  Serial.begin(115200);
   dac.begin();
-  dac.enable();
 
-  // Enable internal reference powered up
-  dac.setReferenceMode(DAC8564::ReferenceMode::REF_POWERED_UP);
+  dac.setReferenceMode(DAC8564::ReferenceMode::REF_DEFAULT); // Set to default mode
+  // dac.setReferenceMode(DAC8564::ReferenceMode::REF_POWERED_DOWN); // Power down internal reference
+  // dac.setReferenceMode(DAC8564::ReferenceMode::REF_POWERED_UP);   // Enable internal reference powered up
 
-  // Set to default mode
-  dac.setReferenceMode(DAC8564::ReferenceMode::REF_DEFAULT);
-
-  // Power down internal reference
-  dac.setReferenceMode(DAC8564::ReferenceMode::REF_POWERED_DOWN);
-
-  // Set all channels to normal operation
-  dac.setOperatingMode(DAC8564::Channel::ALL, DAC8564::OperatingMode::NORMAL_OPERATION);
+  // Set all DACs to normal operation
+  dac.setOperatingModeAll(DAC8564::OperatingMode::NORMAL_OPERATION);
 
   // Write mid-scale to all channels
-  dac.writeAll(32768);
-
-  // Reset DAC
-  dac.reset();
+  dac.broadcast(DAC8564::Broadcast::WRITE_ALL_LOAD_ALL, 32768);
 }
 
 void loop()
 {
-  static uint16_t value = 0;
-  dac.write(DAC8564::Channel::A, value);
-  value += 512;
-  delay(100);
+  // Write mid-scale to channel A of DAC at address 0
+  dac.write(DAC8564::Address::ADDR_0, DAC8564::Load::WRITE_N, DAC8564::Channel::A, 32768);
+  
+  // Write mid-scale to channel B of DAC at address 0 and update (load) output
+  dac.write(DAC8564::Address::ADDR_0, DAC8564::Load::WRITE_N_LOAD_N, DAC8564::Channel::B, 32768);
+  
+  // Write 0 to channel C of DAC at address 0 and update (load) all outputs
+  dac.write(DAC8564::Address::ADDR_0, DAC8564::Load::WRITE_N_LOAD_ALL, DAC8564::Channel::C, 32768);
 }
